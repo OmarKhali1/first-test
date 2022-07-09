a7e5c17bf13d7055885d6489c39042223bae111d
omar <omar.khalil717@hotmail.com>
2022-07-08 21:05:37 -0400

    this is the first commit that i am doing

---
+++ main.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+int main(void) {
+
+  int x;
+  int y;
+  int i=1;
+
+  printf("Enter the command number:\n");
+  printf("0) Exit\n");
+  printf("1) AND\n");
+  printf("2) OR\n");
+  scanf("%d", &i);
+
+  while(i!=0){
+    if(i==0){
+      break;
+    }
+
+    else if(i==1){
+      printf("x=");
+      scanf("%d", &x);
+      
+      while(x!=0 && x!=1){
+        printf("INVALID INPUT\n");
+        printf("x=");
+        scanf("%d", &x);
+      }
+      
+      printf("y="); 
+      scanf("%d", &y);
+
+      while(y!=0 && y!=1){
+        printf("INVALID INPUT\n");
+        printf("y=");
+        scanf("%d", &y);
+      }
+      printf("%d AND %d is %d\n", x, y, x & y);
+      printf("Enter the command number:\n");
+      printf("0) Exit\n");
+      printf("1) AND\n");
+      printf("2) OR\n");
+      scanf("%d", &i);
+    }
+	else if(i==2){
+      printf("x=");
+      scanf("%d", &x);
+      
+      while(x!=0 && x!=1){
+        printf("INVALID INPUT\n");
+        printf("x=");
+        scanf("%d", &x);
+      }
+      
+      printf("y="); 
+      scanf("%d", &y);
+
+      while(y!=0 && y!=1){
+        printf("INVALID INPUT\n");
+        printf("y=");
+        scanf("%d", &y);
+      }
+      printf("%d OR %d is %d\n", x, y, x | y);
+      printf("Enter the command number:\n");
+      printf("0) Exit\n");
+      printf("1) AND\n");
+      printf("2) OR\n");
+      scanf("%d", &i);
+  }
+}
+return 0;
+}
